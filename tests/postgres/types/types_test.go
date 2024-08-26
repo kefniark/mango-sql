c5a7b57f10741dc5e2d7913183b898f1bf38fda1
Kevin Destrem <kefniark@users.noreply.github.com>
2024-08-26 20:27:13 +0900

    Update types_test.go

--- tests/postgres/types/types_test.go
+++ tests/postgres/types/types_test.go
@@ -12,7 +12,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-//go:generate go run ../../../cmd/mangosql/ --package types --logger console ./schema.sql
+//go:generate go run ../../../cmd/mangosql/ --output client.go --package types --logger console ./schema.sql
 
 //go:embed *.sql
 var sqlFS embed.FS
