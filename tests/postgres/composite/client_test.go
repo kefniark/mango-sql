12638f2dcf3f85dcdf433704085a9e29dcfd1b6d
Kevin Destrem <kefniark@users.noreply.github.com>
2024-08-26 20:26:21 +0900

    Update client_test.go

--- tests/postgres/composite/client_test.go
+++ tests/postgres/composite/client_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-//go:generate go run ../../../cmd/mangosql/ --package composite --logger console ./schema.sql
+//go:generate go run ../../../cmd/mangosql/ --output client.go --package composite --logger console ./schema.sql
 
 //go:embed *.sql
 var sqlFS embed.FS
